4f8a872fa05c8178457b5f41557df457999b5cb9
Doanh Duc <doanhthangduc2802@gmail.com>
2022-09-09 09:55:23 +0700

    resolve comment bugs: remove reducer, container, DRY into TodoItem, remove container

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -3,22 +3,23 @@ import { AiOutlinePlus } from "react-icons/ai";
 import { AiOutlineCheck } from "react-icons/ai";
 import { FaTimes } from "react-icons/fa";
 import TodoItem from "../TodoItem/TodoItem";
-import {
-  StyledTodoList,
-  StyledList,
-  HeadContent,
-  InputBtn,
-  InputSection,
-  MonitorBtn,
-} from "./TodoList.styled";
+import { StyledTodoList, StyledList, HeadContent } from "./TodoList.styled";
 
 function TodoList() {
+  let check = true;
+  let unCheck = false;
   return (
     <StyledTodoList>
       <StyledList>
         <HeadContent>Check List</HeadContent>
         <ul>
-          <TodoItem></TodoItem>
+          <TodoItem check background={""}></TodoItem>
+          <TodoItem check background={""}></TodoItem>
+          <TodoItem background={"yellow"}></TodoItem>
+          <TodoItem background={""}></TodoItem>
+          <TodoItem unCheck background={""}></TodoItem>
+          <TodoItem check background={"#6868eb"}></TodoItem>
+          <TodoItem background={""}></TodoItem>
         </ul>
       </StyledList>
     </StyledTodoList>
